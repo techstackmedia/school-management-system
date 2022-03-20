3d359b592546f596b116a37ca5dd6d8c919e27d9
techstackmedia <osagienoah04@gmail.com>
2022-03-20 13:43:16 +0100

    graph timeline

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,27 @@
+import PropTypes from "prop-types";
+import buttonSet from "./Button.module.css";
+const Button = (props: any) => {
+  return (
+    <button
+      className={buttonSet.button}
+      type="button"
+      style={{ backgroundColor: props.background, color: props.color }}
+    >
+      {props.text}
+    </button>
+  );
+};
+
+Button.defaultProps = {
+  color: "#000",
+  background: "#fff",
+  text: "Click",
+};
+
+Button.propTypes = {
+  color: PropTypes.string,
+  background: PropTypes.string,
+  text: PropTypes.string,
+};
+
+export default Button;
